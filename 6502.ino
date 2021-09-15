b901472d1efd32348440f357de02f81778a38230
Daniel Mateos <daniel@mateos.cc>
2021-09-16 02:23:45 +1000

    structure

--- 6502.ino
+++ 6502.ino
@@ -2,6 +2,24 @@
 #define CLOCKPIN 4
 #define CLOCKSPEED 50
 
+void clock_cycle() {
+        digitalWrite(LED_BUILTIN, HIGH);
+        digitalWrite(CLOCKPIN, HIGH);
+        delay(CLOCKSPEED/2);
+        digitalWrite(CLOCKPIN, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(CLOCKSPEED/2);
+}
+
+void init_cpu() {
+        digitalWrite(RESETPIN, LOW);
+	clock_cycle();
+	clock_cycle();
+        digitalWrite(RESETPIN, HIGH);
+
+        delay(100);
+}
+
 void setup() {
         pinMode(RESETPIN, OUTPUT);
         pinMode(CLOCKPIN, OUTPUT);
@@ -12,21 +30,9 @@ void setup() {
         digitalWrite(CLOCKPIN, LOW);
         delay(1000);
 
-        digitalWrite(RESETPIN, LOW);
-        digitalWrite(CLOCKPIN, HIGH);
-        digitalWrite(CLOCKPIN, LOW);
-        delay(100);
-        digitalWrite(CLOCKPIN, HIGH);
-        digitalWrite(CLOCKPIN, LOW);
-        delay(2000);
-        digitalWrite(RESETPIN, HIGH);
+	init_cpu();
 }
 
 void loop() {
-        digitalWrite(LED_BUILTIN, HIGH);
-        digitalWrite(CLOCKPIN, HIGH);
-        delay(CLOCKSPEED/2);
-        digitalWrite(CLOCKPIN, LOW);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(CLOCKSPEED/2);
+	clock_cycle();
 }

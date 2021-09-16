a9eddae007d5b01a4ff29580b6a11f0ff41fe2a5
Daniel Mateos <daniel@mateos.cc>
2021-09-16 23:22:05 +1000

    add adress/data lines + possible reading code + serial connection

--- 420-6502.ino
+++ 420-6502.ino
@@ -1,37 +1,89 @@
+#include <SoftwareSerial.h>
+
 #define RESETPIN 3
 #define CLOCKPIN 4
 #define CLOCKSPEED 50
 
+enum data_pins {
+    DATAPIN_0 = 12,
+    DATAPIN_1,
+    DATAPIN_2,
+    DATAPIN_3,
+    DATAPIN_4,
+    DATAPIN_5,
+    DATAPIN_6,
+    DATAPIN_7,
+};
+
+enum address_pins {
+    ADDRESSPIN_0 = 24,
+    ADDRESSPIN_1,
+    ADDRESSPIN_2,
+    ADDRESSPIN_3,
+    ADDRESSPIN_4,
+    ADDRESSPIN_5,
+    ADDRESSPIN_6,
+    ADDRESSPIN_7,
+    ADDRESSPIN_8,
+    ADDRESSPIN_9,
+    ADDRESSPIN_10,
+    ADDRESSPIN_11,
+    ADDRESSPIN_12,
+    ADDRESSPIN_13,
+    ADDRESSPIN_14,
+    ADDRESSPIN_15,
+};
+
+// Serial connection to relay stuff back to computer over USB
+SoftwareSerial serial(2, 3);
+
 void clock_cycle() {
-        digitalWrite(LED_BUILTIN, HIGH);
-        digitalWrite(CLOCKPIN, HIGH);
-        delay(CLOCKSPEED/2);
-        digitalWrite(LED_BUILTIN, LOW);
-        digitalWrite(CLOCKPIN, LOW);
-        delay(CLOCKSPEED/2);
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(CLOCKPIN, HIGH);
+    delay(CLOCKSPEED / 2);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(CLOCKPIN, LOW);
+    delay(CLOCKSPEED / 2);
+}
+
+void read_address_pins(unsigned char *adio) {
+    for (int i = 0; i < 8; i++) {
+        adio[i] = digitalRead(ADDRESSPIN_0 + i);
+    }
 }
 
 void init_cpu() {
-	// Reset the CPU
-        digitalWrite(RESETPIN, LOW);
-	clock_cycle();
-	clock_cycle();
-        digitalWrite(RESETPIN, HIGH);
+    // Reset the CPU
+    digitalWrite(RESETPIN, LOW);
+    clock_cycle();
+    clock_cycle();
+    digitalWrite(RESETPIN, HIGH);
 }
 
 void setup() {
-	// Setup initial state
-        pinMode(RESETPIN, OUTPUT);
-        pinMode(CLOCKPIN, OUTPUT);
-        pinMode(LED_BUILTIN, OUTPUT);
+    // Setup initial state
+    pinMode(RESETPIN, OUTPUT);
+    pinMode(CLOCKPIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-        digitalWrite(LED_BUILTIN, LOW);
-        digitalWrite(RESETPIN, HIGH);
-        digitalWrite(CLOCKPIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(RESETPIN, HIGH);
+    digitalWrite(CLOCKPIN, LOW);
 
-	init_cpu();
+    // Setup serial connection back to computer
+    Serial.begin(9600);
+    while (!Serial) {
+        continue;
+    }
+
+    init_cpu();
 }
 
 void loop() {
-	clock_cycle();
+    unsigned char adio[8];
+
+    clock_cycle();
+    read_address_pins(adio);
+
+    serial.write("clock tick");
 }

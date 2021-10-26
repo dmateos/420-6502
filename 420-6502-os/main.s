c755c174ef3446c88d48ee7352ebcb2ae86f6dd4
Daniel Mateos <daniel@mateos.cc>
2021-10-27 02:06:47 +1100

    jmp program test

--- 420-6502-os/main.s
+++ 420-6502-os/main.s
@@ -2,19 +2,19 @@
 
 .PROC Main
 loop:	JSR NopTest
-		JMP loop
+	JMP loop
 .ENDPROC
 
 .PROC NopTest
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		RTS
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	RTS
 .ENDPROC

fee4065dda6cd59295719159554c234e48b890c4
Daniel Mateos <daniel@mateos.cc>
2021-10-29 23:41:50 +1100

    refactor

--- 420-6502-os/main.s
+++ 420-6502-os/main.s
@@ -1,17 +1,15 @@
 .segment "kernel"
 
 .PROC Main
-loop:   
-        JSR MagicRegisterTest
-        JMP loop
+loop:       JSR MagicRegisterTest
+            JMP loop
 .ENDPROC
 
 .PROC MagicRegisterTest
-        LDX #$10        ;How many times do we want to loop?
-loop:   
-        LDY #$69
-        STY $F420
-        DEX             ;Dec counter
-        BNE loop        ;Loop until counter is 0
-        RTS
+            LDX #$10        ;How many times do we want to loop?
+loop:       LDY #$69
+            STY $F420
+            DEX             ;Dec counter
+            BNE loop        ;Loop until counter is 0
+            RTS
 .ENDPROC

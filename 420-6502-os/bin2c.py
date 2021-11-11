ecd4d9b3352237fb1cf0e629aef98f2019ec9f47
Daniel Mateos <daniel@mateos.cc>
2021-11-11 17:06:05 +1100

    refactor

--- 420-6502-os/bin2c.py
+++ 420-6502-os/bin2c.py
@@ -9,8 +9,10 @@ if not options.filename:
     print("specify filename")
     sys.exit(1)
 
+innerstr = ""
 with open(options.filename, mode="rb") as f:
-    file_data = f.read()
-    print(file_data)
+    for byte in f.read():
+        innerstr += f"{hex(byte)},"
 
+print(f"const uint8_t kernel_program[] = {{{innerstr}}};")
 sys.exit(0)

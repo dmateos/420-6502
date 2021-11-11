d7454a51f5326123cdef9fd0f43143eda9b0cb38
Daniel Mateos <daniel@mateos.cc>
2021-11-11 21:57:14 +1100

    refactor

--- 420-6502-os/bin2c.py
+++ 420-6502-os/bin2c.py
@@ -9,16 +9,16 @@ if not options.filename:
     print("specify filename")
     sys.exit(1)
 
+innerstr = ""
+size = 0
+
 try:
-    innerstr = ""
-    size = 0
     with open(options.filename, mode="rb") as f:
         for byte in f.read():
             innerstr += f"{hex(byte)},"
             size += 1
-
-    print(f"const uint8_t program[] = {{{innerstr}}};\nconst uint32_t program_size = {size};")
-    sys.exit(0)
 except FileNotFoundError:
-    print("file not found")
-    sys.exit(1)
+    pass
+
+print(f"const uint8_t program[] = {{{innerstr}}};\nconst uint32_t program_size = {size};")
+sys.exit(0)

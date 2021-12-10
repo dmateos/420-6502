8af041d28ae6862f651fb6411861a17e4a8cdf29
Daniel Mateos <daniel@mateos.cc>
2021-12-10 23:28:47 +1100

    display init code

--- 420-6502-avr/gpu.ino
+++ 420-6502-avr/gpu.ino
@@ -1,7 +1,14 @@
-#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
 
-int write_display(char c) {
-  return 0;
+#define SCREEN_WIDTH 128  // OLED display width,  in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup_display() {
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (true)
+      ;
+  }
 }

c10fe768b0dca47e6231eae231b7c5874b55f930
Daniel Mateos <daniel@mateos.cc>
2021-12-11 03:48:03 +1100

    gfx tweak to help stop OLED burnins

--- 420-6502-avr/gpu.ino
+++ 420-6502-avr/gpu.ino
@@ -6,15 +6,24 @@
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+static uint8_t screen_phase = 0;
+
 void write_display_char(char c) {
   oled.write(c);
   oled.display();
 }
 
 void clear_display() {
-  oled.setCursor(0, 0);
   oled.clearDisplay();
-  oled.println("420-6502 BIOS 0.1");
+  if (screen_phase == 0) {
+    screen_phase = 1;
+    oled.setCursor(0, 0);
+    oled.println("420-6502 BIOS 0.1");
+  } else {
+    screen_phase = 0;
+    oled.setCursor(5, 0);
+    oled.println("Blaze it!");
+  }
 }
 
 void setup_display() {

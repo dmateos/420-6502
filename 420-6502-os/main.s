496fa1d493bbf25e540d5ee59744044cad8141d6
Daniel Mateos <daniel@mateos.cc>
2021-12-12 05:31:56 +1100

    asm optimisationsl

--- 420-6502-os/main.s
+++ 420-6502-os/main.s
@@ -1,70 +1,61 @@
+.FEATURE STRING_ESCAPES
 .segment "kernel"
 
 .proc Main
-loop:       JSR Header
-            JSR HelloWorldTest
-            
+loop:       JSR PrintHeader
+            JSR PrintHelloWorldTest
+            JSR PrintHelloWorldTest
+            JSR PrintHelloWorldTest
+            JSR PrintCountTest
+            JSR PrintCountTest
+            JSR PrintCountTest
+
             LDY #$FF                ;Clear display
             STY $F420
 
             JMP loop
 .endproc
 
-.proc Header
-            LDY #$6D                ;m
-            STY $F420
-            LDY #$61                ;a
-            STY $F420
-            LDY #$74                ;t
-            STY $F420
-            LDY #$65                ;e
-            STY $F420
-            LDY #$4F                ;O
-            STY $F420
-            LDY #$53                ;S
-            STY $F420
-            LDY #$20                ;space
-            STY $F420
-            LDY #$76                ;v
-            STY $F420
-            LDY #$30                ;0
-            STY $F420
-            LDY #$2E                ;.
-            STY $F420
-            LDY #$31                ;1
-            STY $F420
-            LDY #$0A                ;\n
-            STY $F420
+HeaderMsg:  .byte "mateOS v0.1\n"
+HeaderMsgLen = 12
+
+.proc PrintHeader
+            LDX #0              
+loop:       CPX #HeaderMsgLen       ;Loop untill we reach end of msg
+            BEQ done                ;
+            LDY HeaderMsg,X         ;Load message offset ito Y
+            STY $F420               ;Print
+            INX                     ;
+            JMP loop                ;Keep going
+done:
             RTS
 .endproc
 
-.proc HelloWorldTest
-            LDX #$6                 ;How many times do we want to loop?
-loop:       LDY #$68                ;h
-            STY $F420
-            LDY #$65                ;e
-            STY $F420
-            LDY #$6C                ;l
-            STY $F420
-            LDY #$6C                ;l
-            STY $F420
-            LDY #$6F                ;o
-            STY $F420
-            LDY #$20                ;space
-            STY $F420
-            LDY #$77                ;w
-            STY $F420
-            LDY #$6F                ;o
-            STY $F420
-            LDY #$72                ;r
-            STY $F420
-            LDY #$6C                ;l
-            STY $F420
-            LDY #$64                ;d
-            STY $F420
+HelloMsg:   .byte "Hello World!\n"
+HelloMsgLen = 13
+
+.proc PrintHelloWorldTest
+            LDX #0              
+loop:       CPX #HelloMsgLen        ;Loop untill we reach end of msg
+            BEQ done                ;
+            LDY HelloMsg,X          ;Load message offset ito Y
+            STY $F420               ;Print
+            INX                     ;
+            JMP loop                ;Keep going
+done:
+            RTS
+.endproc
+
+.proc PrintCountTest
+            LDX #$A                 ;Loop 10 times
+            LDY #$30                ;Start at 0
+
+loop:       STY $F420               ;Print number 
+            INY                     ;Inc number
+            DEX                     ;Dec loop counter
+            BNE loop                ;Keep looping
+
             LDY #$0A                ;\n
-            STY $F420
-            DEX                     ;Dec counter
-            BNE loop                ;Loop until counter is 0
+            STY $F420               ;Print newline
             RTS
 .endproc

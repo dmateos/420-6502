497bf78f75286d48a5d963dc1faffee8da1b6403
Daniel Mateos <daniel@mateos.cc>
2021-12-11 20:18:27 +1100

    playing around with printing

--- 420-6502-os/main.s
+++ 420-6502-os/main.s
@@ -1,12 +1,46 @@
 .segment "kernel"
 
 .proc Main
-loop:       JSR MagicRegisterTest
+loop:       JSR Header
+            JSR HelloWorldTest
+            
+            LDY #$FF        ;Clear display
+            STY $F420
+
             JMP loop
 .endproc
 
-.proc MagicRegisterTest
-            LDX #$7         ;How many times do we want to loop?
+.proc Header
+    LDY #$6D                ;m
+    STY $F420
+    LDY #$61                ;a
+    STY $F420
+    LDY #$74                ;t
+    STY $F420
+    LDY #$65                ;e
+    STY $F420
+    LDY #$4F                ;O
+    STY $F420
+    LDY #$53                ;S
+    STY $F420
+    LDY #$20                ;space
+    STY $F420
+    LDY #$74                ;v
+    STY $F420
+    LDY #$30                ;0
+    STY $F420
+    LDY #$2E                ;.
+    STY $F420
+    LDY #$31                ;1
+    STY $F420
+    LDY #$0A                ;\n
+    STY $F420
+    RTS
+.endproc
+
+
+.proc HelloWorldTest
+            LDX #$6         ;How many times do we want to loop?
 loop:       LDY #$68        ;h
             STY $F420
             LDY #$65        ;e
@@ -33,8 +67,5 @@ loop:       LDY #$68        ;h
             STY $F420
             DEX             ;Dec counter
             BNE loop        ;Loop until counter is 0
-
-            LDY #$FF        ;Clear display
-            STY $F420
             RTS
 .endproc

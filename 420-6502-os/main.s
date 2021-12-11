2fee6f1d3d3dbe5a9472bcabf9051ad58a1cb4a2
Daniel Mateos <daniel@mateos.cc>
2021-12-11 22:27:19 +1100

    tweaks

--- 420-6502-os/main.s
+++ 420-6502-os/main.s
@@ -4,68 +4,67 @@
 loop:       JSR Header
             JSR HelloWorldTest
             
-            LDY #$FF        ;Clear display
+            LDY #$FF                ;Clear display
             STY $F420
 
             JMP loop
 .endproc
 
 .proc Header
-    LDY #$6D                ;m
-    STY $F420
-    LDY #$61                ;a
-    STY $F420
-    LDY #$74                ;t
-    STY $F420
-    LDY #$65                ;e
-    STY $F420
-    LDY #$4F                ;O
-    STY $F420
-    LDY #$53                ;S
-    STY $F420
-    LDY #$20                ;space
-    STY $F420
-    LDY #$74                ;v
-    STY $F420
-    LDY #$30                ;0
-    STY $F420
-    LDY #$2E                ;.
-    STY $F420
-    LDY #$31                ;1
-    STY $F420
-    LDY #$0A                ;\n
-    STY $F420
-    RTS
+            LDY #$6D                ;m
+            STY $F420
+            LDY #$61                ;a
+            STY $F420
+            LDY #$74                ;t
+            STY $F420
+            LDY #$65                ;e
+            STY $F420
+            LDY #$4F                ;O
+            STY $F420
+            LDY #$53                ;S
+            STY $F420
+            LDY #$20                ;space
+            STY $F420
+            LDY #$76                ;v
+            STY $F420
+            LDY #$30                ;0
+            STY $F420
+            LDY #$2E                ;.
+            STY $F420
+            LDY #$31                ;1
+            STY $F420
+            LDY #$0A                ;\n
+            STY $F420
+            RTS
 .endproc
 
-
 .proc HelloWorldTest
-            LDX #$6         ;How many times do we want to loop?
-loop:       LDY #$68        ;h
+            LDX #$6                 ;How many times do we want to loop?
+loop:       LDY #$68                ;h
             STY $F420
-            LDY #$65        ;e
+            LDY #$65                ;e
             STY $F420
-            LDY #$6C        ;l
+            LDY #$6C                ;l
             STY $F420
-            LDY #$6C        ;l
+            LDY #$6C                ;l
             STY $F420
-            LDY #$6F        ;o
+            LDY #$6F                ;o
             STY $F420
-            LDY #$20        ;space
+            LDY #$20                ;space
             STY $F420
-            LDY #$77        ;w
+            LDY #$77                ;w
             STY $F420
-            LDY #$6F        ;o
+            LDY #$6F                ;o
             STY $F420
-            LDY #$72        ;r
+            LDY #$72                ;r
             STY $F420
-            LDY #$6C        ;l
+            LDY #$6C                ;l
             STY $F420
-            LDY #$64        ;d
+            LDY #$64                ;d
             STY $F420
-            LDY #$0A        ;\n
+            LDY #$0A                ;\n
             STY $F420
-            DEX             ;Dec counter
-            BNE loop        ;Loop until counter is 0
+            DEX                     ;Dec counter
+            BNE loop                ;Loop until counter is 0
             RTS
 .endproc
